0f2fd507ac82e55393aecad50f137a8211241c67
hackbox4sk <90349691+hackbox4sk@users.noreply.github.com>
2021-11-22 19:06:24 +0530

    Update and rename hackingtool.py to hackbox.py

--- hackbox.py
+++ hackbox.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
                         [1] Manual 
                         [2] Default
                 """)
-                choice = input("Z4nzu =>> ")
+                choice = input("HackBox =>> ")
 
                 if choice == "1":
                     inpath = input("Enter Path (with Directory Name) >> ")
